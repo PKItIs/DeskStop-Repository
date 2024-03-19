0573423030e169586a3c3d76acb3109747f6ef1f
PKItIs <163971711+PKItIs@users.noreply.github.com>
2024-03-19 13:01:29 +0530

    Add files via upload

---
+++ ADV C56/main.js
@@ -0,0 +1,4 @@
+document.getElementById("heading1").style.color = "grey";
+
+// Add this line to apply the CSS directly from the main.js file
+//document.getElementById("heading1").style.cssText = "color: yellow;";
